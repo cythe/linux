8c4eada9a20d526ad02cee8eaf6b7ccc2bb8eaf8
Vladimir Oltean <vladimir.oltean@nxp.com>
2023-03-16 20:39:51 +0200

    net: enetc: add support for TSN capabilities through tsntool
    commit  177f9b5d63946040152cd8cd248e2865627b7525 from
    https://github.com/nxp-imx/linux-imx lf-6.12.y
    
    Support Qbv/Qci/Qbu/Credit Base Shaper etc.
    This patch using the generic netlink layer net/tsn/* and
    include/net/tsn.h interface load by user space. The user space
    refer the include/uapi/linux/tsn.h.
    
    Signed-off-by: Po Liu <Po.Liu@nxp.com>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- drivers/net/ethernet/freescale/enetc/enetc.c
+++ drivers/net/ethernet/freescale/enetc/enetc.c
@@ -25,8 +25,8 @@ void enetc_port_mac_wr(struct enetc_si *si, u32 reg, u32 val)
 }
 EXPORT_SYMBOL_GPL(enetc_port_mac_wr);
 
-static void enetc_change_preemptible_tcs(struct enetc_ndev_priv *priv,
-					 u8 preemptible_tcs)
+void enetc_change_preemptible_tcs(struct enetc_ndev_priv *priv,
+					u8 preemptible_tcs)
 {
 	if (!(priv->si->hw_features & ENETC_SI_F_QBU))
 		return;
@@ -34,6 +34,7 @@ static void enetc_change_preemptible_tcs(struct enetc_ndev_priv *priv,
 	priv->preemptible_tcs = preemptible_tcs;
 	enetc_mm_commit_preemptible_tcs(priv);
 }
+EXPORT_SYMBOL_GPL(enetc_change_preemptible_tcs);
 
 static int enetc_num_stack_tx_queues(struct enetc_ndev_priv *priv)
 {
