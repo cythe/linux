7c5d46bcf1230d01f522f9ee8f30692766e4f81e
Meng Li <Meng.Li@windriver.com>
2024-09-19 11:02:19 +0800

    drivers: media: enforce camera driver to be loaded before media controller driver
    On some imx platforms, for example, imx93-evk platform, media controller
    driver can initialize successfully only when camera driver has be loaded
    before it. Because camera needs to be registered as a sensor subdevice
    during initializing media controller driver. Only in this way, camera
    feature can work fine. So, add a dependency relationship with macro
    MODULE_SOFTDEP.
    
    Signed-off-by: Meng Li <Meng.Li@windriver.com>
    Signed-off-by: Ruiqiang Hao <Ruiqiang.Hao@windriver.com>

--- drivers/staging/media/imx/imx8-media-dev.c
+++ drivers/staging/media/imx/imx8-media-dev.c
@@ -1175,3 +1175,4 @@ MODULE_AUTHOR("Freescale Semiconductor, Inc.");
 MODULE_DESCRIPTION("MXC Media Device driver");
 MODULE_LICENSE("GPL");
 MODULE_ALIAS("platform:" MXC_MD_DRIVER_NAME);
+MODULE_SOFTDEP("pre: ap1302");
